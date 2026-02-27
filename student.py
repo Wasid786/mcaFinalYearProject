40df590c47d7735fc36ac163e16e1fbb21086be5
Wasid Ansari <114327679+Wasid786@users.noreply.github.com>
2026-02-27 23:30:47 +0530

    student right section complete

--- student.py
+++ student.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from turtle import bgcolor
 from PIL import Image, ImageTk
 from matplotlib import widgets
+from matplotlib.hatch import HorizontalHatch
 from numpy import imag, save
 from pyparsing import col
 
@@ -336,7 +337,103 @@ class Student:
 
         # /////  right label frame   ////////////////
         Right_frame= LabelFrame(main_frame, bd=2, relief=RIDGE, text="Right Frame",font=("times new roman", 35, "bold"),
-              bg="white", fg="red").place(x=780, y=10, width=690, height=580)
+              bg="white", fg="red")
+        Right_frame.place(x=780, y=10, width=720, height=580)
+        
+                # IMAGE
+        img_right = Image.open(r"D:\Projects\mcaProject\static\images\img06.jpg")
+        img_right = img_right.resize((720, 130), Image.Resampling.LANCZOS)
+        self.photoimg_right = ImageTk.PhotoImage(img_right)
+
+        f_lbl = Label(Right_frame, image=self.photoimg_right)
+        f_lbl.place(x=5, y=135, width=720, height=70)
+
+
+        # /////////// search feature //////////
+        search_frame= LabelFrame(Right_frame, bd=2, relief=RIDGE, text="Class Student Info",font=("times new roman", 15, "bold"),
+              bg="white", fg="red")
+        search_frame.place(x=5, y=135, width=710, height=70)
+        search_label = Label(search_frame, text="Search By: ", font=("times new roman", 13, "bold"), bg="red",fg="black" )
+        search_label.grid(row=0, column=0, padx=12, pady=5, sticky=W)
+
+
+        search_combo = ttk.Combobox(
+           search_frame,
+           font=("times new roman", 12, "bold"),
+          state="readonly",
+          width=13
+        )
+        search_combo["values"] = ("Select", "Roll_No", "Phone_No")
+        search_combo.current(0)
+        search_combo.grid(row=0, column=1, padx=2, pady=10, sticky=W)
+
+        search_entry = ttk.Entry(search_frame, width=15, font=("times new roman",13, "bold"))
+        search_entry.grid(row=0, column=2, padx=10, pady=5, sticky=W)
+
+
+
+
+        search_btn= Button(search_frame, text="Search", width=14,  font=("times new roman",13, "bold"), bg="blue", fg="white")
+        search_btn.grid(row=0, column=3, padx=10, pady=5)
+        
+        showAll_btn= Button(search_frame, text="Show All", width=14,  font=("times new roman",13, "bold"), bg="blue", fg="white")
+        showAll_btn.grid(row=0, column=4, padx=10, pady=5)
+
+        # ///////// table ///////
+        table_frame = LabelFrame(Right_frame, bd=2, bg="white", relief=RIDGE)
+        table_frame.place(x=5, y=210, width=710, height=350)
+
+        scroll_x = ttk.Scrollbar(table_frame, orient=HORIZONTAL)
+        scroll_y = ttk.Scrollbar(table_frame, orient=VERTICAL)
+
+        self.student_table = ttk.Treeview(
+        table_frame,
+        columns=("dep","course","year","sem","id","name","div","roll","gender","dob","email","phone","address","teacher","photo"),
+        xscrollcommand=scroll_x.set,
+        yscrollcommand=scroll_y.set
+        )
+
+#  CONNECT scrollbars
+        scroll_x.config(command=self.student_table.xview)
+        scroll_y.config(command=self.student_table.yview)
+
+#  PROPER PACKING
+        scroll_x.pack(side=BOTTOM, fill=X)
+        scroll_y.pack(side=RIGHT, fill=Y)
+
+        self.student_table.pack(fill=BOTH, expand=1)
+
+#  HEADINGS
+        self.student_table.heading("dep", text="Department")
+        self.student_table.heading("course", text="Course")
+        self.student_table.heading("year", text="Year")
+        self.student_table.heading("sem", text="Semester")
+        self.student_table.heading("id", text="Student ID")
+        self.student_table.heading("name", text="Name")
+        self.student_table.heading("div", text="Division")
+        self.student_table.heading("roll", text="Roll No")
+        self.student_table.heading("gender", text="Gender")
+        self.student_table.heading("dob", text="DOB")
+        self.student_table.heading("email", text="Email")
+        self.student_table.heading("phone", text="Phone")
+        self.student_table.heading("address", text="Address")
+        self.student_table.heading("teacher", text="Teacher")
+        self.student_table.heading("photo", text="Photo")
+
+        self.student_table["show"] = "headings"
+
+#  COLUMN WIDTH (make wide to force horizontal scroll)
+        for col in ("dep","course","year","sem","id","name","div","roll","gender","dob","email","phone","address","teacher","photo"):
+         self.student_table.column(col, width=120)
+
+
+        
+
+
+
+
+        
+
 
 if __name__ == "__main__":
     root = Tk()
